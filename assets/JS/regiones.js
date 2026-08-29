194a5f233c970b6e4d84fd9e255c648a1b13e28e
Mun-oz <mar.munozm@duocuc.cl>
2026-08-29 19:06:50 -0400

    feat: agregar arreglo de regiones y comunas en regiones.js y vincular con registro.html

---
+++ assets/JS/regiones.js
@@ -0,0 +1,14 @@
+const datosRegiones = [
+    {
+        region: "Región Metropolitana de Santiago",
+        comunas: ["Santiago", "Providencia", "Las Condes", "Maipú", "Puente Alto", "Ñuñoa", "La Florida"]
+    },
+    {
+        region: "Valparaíso",
+        comunas: ["Valparaíso", "Viña del Mar", "Concón", "Quilpué", "Villa Alemana"]
+    },
+    {
+        region: "Biobío",
+        comunas: ["Concepción", "Talcahuano", "San Pedro de la Paz", "Chiguayante", "Los Ángeles"]
+    }
+];
